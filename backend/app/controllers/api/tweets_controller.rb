364ee0c0ed2b826fcb842f52b2329f22f8e69aa2
kenmazaika <ken@thefirehoseproject.com>
2020-06-02 11:46:39 -0700

    Generate Tweet model and build an API to expose them.

---
+++ backend/app/controllers/api/tweets_controller.rb
@@ -0,0 +1,36 @@
+class Api::TweetsController < ApplicationController
+  def index
+    tweets = Tweet.all
+    render json: tweets
+  end
+
+  def create
+    tweet = Tweet.create(tweet_params)
+    render json: tweet
+  end
+
+  def show
+    tweet = Tweet.find(params[:id])
+    render json: tweet
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    render json: tweet
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    render json: {}
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :author)
+  end
+
+
+end
