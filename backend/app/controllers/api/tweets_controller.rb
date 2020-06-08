0e25ff9b235fd41150660ccf2f13533da7a94ecc
kenmazaika <ken@thefirehoseproject.com>
2020-06-08 12:08:16 -0700

    Adjust API response.

--- backend/app/controllers/api/tweets_controller.rb
+++ backend/app/controllers/api/tweets_controller.rb
@@ -2,7 +2,7 @@ class Api::TweetsController < ActionController::API
   def index
     tweets = Tweet.all
     # render json: tweets
-    render json: "Yolo Swag x 6"
+    render json: "Yolo Swag x 7"
   end
 
   def create
